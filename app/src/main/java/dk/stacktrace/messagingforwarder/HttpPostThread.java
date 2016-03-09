411bcaf8907e3c4a2edf868fdeecaa914eff71f0
Kasper Laudrup <laudrup@stacktrace.dk>
2016-03-09 19:05:55 +0100

    Remove unused imports

--- app/src/main/java/dk/stacktrace/messagingforwarder/HttpPostThread.java
+++ app/src/main/java/dk/stacktrace/messagingforwarder/HttpPostThread.java
@@ -1,12 +1,9 @@
 package dk.stacktrace.messagingforwarder;
 
 import android.util.Log;
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
