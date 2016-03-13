1b701da2e06d02ec13445f7df3a339aea08d9c59
Kasper Laudrup <laudrup@stacktrace.dk>
2016-03-13 17:57:31 +0100

    Ask for SMS receive permissions at runtime
    Instead of expecting the user to manually grant SMS_RECEIVE
    permissions ask the user at runtime to grant the app this permission.
    
    This probably only work with Android 6 and above, not sure how it
    works on earlier platforms.

--- app/src/main/java/dk/stacktrace/messagingforwarder/SettingsActivity.java
+++ app/src/main/java/dk/stacktrace/messagingforwarder/SettingsActivity.java
@@ -1,19 +1,43 @@
 package dk.stacktrace.messagingforwarder;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
 public class SettingsActivity extends PreferenceActivity {
     private static final String TAG = IncomingMessageReceiver.class.getName();
+    private static final int REQUEST_RECEIVE_SMS = 1;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         Log.i(TAG, "Creating settings activity");
         super.onCreate(savedInstanceState);
         getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "Requesting permissions to receive SMS messages");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECEIVE_SMS}, REQUEST_RECEIVE_SMS);
+        }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int code, String permissions[], int[] results) {
+        if (code != REQUEST_RECEIVE_SMS) {
+            Log.w(TAG, "Unexpected request code received: " + code);
+            return;
+        }
+        if (results.length > 0 && results[0] == PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "RECEIVE_SMS permission granted. Will forward SMS messages");
+        } else {
+            Log.i(TAG, "RECEIVE_SMS permission denied. No messages will be forwarded");
+        }
+    }
+
     public static class SettingsFragment extends PreferenceFragment
     {
         @Override
